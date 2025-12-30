906655c966b9d80866c99448b4f721d9ab7b0314
KnutMQS <knutmqs@gmail.com>
2025-12-30 11:28:47 +0100

    Add HeroContent component with introductory text and styling

--- client/src/components/hero/HeroContent.jsx
+++ client/src/components/hero/HeroContent.jsx
@@ -1,5 +1,15 @@
 function HeroContent() {
-  return <div></div>;
+  return (
+    <div className="relative z-10 text-center max-w-3xl">
+      <h1 className="text-5xl md:text-7xl font-bold tracking-tight mb-4">
+        Rookie <br />
+        <span className="text-transparent bg-clip-text bg-linear-to-r from-violet-500 to-fuchsia-500">
+          Front-End <br />
+        </span>
+        Dev
+      </h1>
+    </div>
+  );
 }
 
 export default HeroContent;
