e66482b708d7ce7803cd16578d035b27aef6821c
KnutMQS <knutmqs@gmail.com>
2025-12-30 15:08:15 +0100

    Add margin-top to HeroSection for improved layout spacing

--- client/src/components/hero/HeroSection.jsx
+++ client/src/components/hero/HeroSection.jsx
@@ -2,7 +2,7 @@ import HeroContent from "./HeroContent";
 
 function HeroSection() {
   return (
-    <section className="relative flex flex-col min-h-screen items-center justify-center px-6 bg-bg overflow-hidden">
+    <section className="relative mt-25 flex flex-col min-h-screen items-center justify-center px-6 bg-bg overflow-hidden">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-51 h-51 rounded-4xl bg-radial-[at_25%_25%] from-blue-800 to-teal-800 to-75% blur-3xl " />
       <HeroContent />
     </section>
