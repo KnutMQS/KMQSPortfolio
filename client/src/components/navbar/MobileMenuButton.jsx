7609bc8f6307dfb111c93cdb58ae096a99cd14cf
KnutMQS <knutmqs@gmail.com>
2025-12-30 12:11:54 +0100

    Update MobileMenuButton and Navbar styles for improved visual consistency

--- client/src/components/navbar/MobileMenuButton.jsx
+++ client/src/components/navbar/MobileMenuButton.jsx
@@ -4,20 +4,20 @@ function MobileMenuButton({ isOpen, onClick }) {
       onClick={onClick}
       aria-expanded={isOpen}
       aria-label="Toggle mobile menu"
-      className="group relative left-25 z-50 flex h-8 w-8 flex-col items-center justify-center rounded-md border-2 border-fuchsia-700 bg-bg p-2 focus:outline-none"
+      className="group relative z-50 flex h-8 w-8 flex-col items-center justify-center rounded-md border-2 border-fuchsia-500 bg-bg p-2 focus:outline-none"
     >
       <div
-        className={`h-0.5 w-6 rounded bg-fuchsia-700 transition-all duration-300 ease-out ${
+        className={`h-0.5 w-6 rounded bg-fuchsia-500 transition-all duration-300 ease-out ${
           isOpen ? "translate-y-0.5 rotate-45" : "-translate-y-0.5"
         }`}
       />
       <div
-        className={`h-0.5 w-6 rounded bg-fuchsia-700 transition-all duration-300 ease-out ${
+        className={`h-0.5 w-6 rounded bg-fuchsia-500 transition-all duration-300 ease-out ${
           isOpen ? "opacity-0" : "opacity-100"
         }`}
       />
       <div
-        className={`h-0.5 w-6 rounded bg-fuchsia-700 transition-all duration-300 ease-out ${
+        className={`h-0.5 w-6 rounded bg-fuchsia-500 transition-all duration-300 ease-out ${
           isOpen ? "-translate-y-0.5 -rotate-45" : "translate-y-0.5"
         }`}
       />
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -9,8 +9,8 @@ function Navbar() {
   const toggleMenu = () => setIsOpen(!isOpen);
   const closeMenu = () => setIsOpen(false);
   return (
-    <nav className="sticky top-0 z-50 flex w-full items-center justify-between border-solid border-b-2 bg-bg border-fuchsia-800 px-3 py-4 shadow-sm">
-      <div className="mx-auto flex max-w-7xl items-center justify-around">
+    <nav className="sticky top-0 z-50 w-full bg-bg px-3 py-4 shadow-sm after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:bg-linear-to-r after:from-violet-500 after:to-fuchsia-500">
+      <div className="mx-auto flex max-w-7xl items-center justify-between">
         <Link
           to="/"
           className="text-2xl font-bold text-transparent bg-clip-text bg-linear-to-r from-violet-500 to-fuchsia-500"
@@ -18,7 +18,7 @@ function Navbar() {
         >
           KMQS
         </Link>
-        <div className="hidden md:block">
+        <div className="hidden md:block md:gap-4 md:justify-center md:items-center md:mx-auto">
           <NavLinks />
         </div>
         <div className="md:hidden">
