2788a4d6f462e14a75e06792e290ce24cce3c7da
KnutMQS <knutmqs@gmail.com>
2025-12-30 11:28:36 +0100

    Add HeroSection route to App component for homepage display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import HeroSection from "./components/hero/HeroSection";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+      </Routes>
     </Router>
   );
 }
