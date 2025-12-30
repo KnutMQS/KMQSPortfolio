706a17aa884056d5285e75a1bb485e0579495c41
KnutMQS <knutmqs@gmail.com>
2025-12-30 11:28:57 +0100

    Refactor HeroSection to use HeroContent and improve layout structure

--- client/src/components/hero/HeroSection.jsx
+++ client/src/components/hero/HeroSection.jsx
@@ -1,8 +1,11 @@
+import HeroContent from "./HeroContent";
+
 function HeroSection() {
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <section className="relative flex flex-col min-h-screen items-center justify-center px-6 bg-bg overflow-hidden">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-31 h-31 rounded-4xl bg-radial-[at_25%_25%] from-violet-500 to-fuchsia-500 to-75% blur-3xl " />
+      <HeroContent />
+    </section>
   );
 }
 
