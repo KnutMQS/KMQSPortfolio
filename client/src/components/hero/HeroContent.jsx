cc53c7ea8988dfa7e0914cee8b6d443a476a4766
KnutMQS <knutmqs@gmail.com>
2026-01-02 15:43:58 +0100

    Added <p> with dummy text

--- client/src/components/hero/HeroContent.jsx
+++ client/src/components/hero/HeroContent.jsx
@@ -1,13 +1,19 @@
 function HeroContent() {
   return (
-    <div className="relative z-10 text-center max-w-3xl">
-      <h1 className="text-5xl md:text-7xl font-bold tracking-tight mb-4">
-        Rookie <br />
-        <span className="text-transparent bg-clip-text bg-linear-to-r from-violet-500 to-fuchsia-500">
-          Front-End <br />
-        </span>
-        Dev
-      </h1>
+    <div>
+      <div className="relative z-10 text-center max-w-3xl">
+        <h1 className="text-5xl md:text-7xl font-bold tracking-tight mb-4">
+          Rookie <br />
+          <span className="text-transparent bg-clip-text bg-linear-to-r from-violet-500 to-fuchsia-500">
+            Front-End <br />
+          </span>
+          Dev
+        </h1>
+        <p className="text-xl tracking-tight text-shadow-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quasi
+          optio minus error rem voluptas doloremque nisi cum quis minima?
+        </p>
+      </div>
     </div>
   );
 }
