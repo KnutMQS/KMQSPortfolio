146935fa97f09c8f561b3831d83e0302f0539296
KnutMQS <knutmqs@gmail.com>
2026-01-02 14:02:36 +0100

    Refactor Navbar component for improved layout and structure

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -9,20 +9,24 @@ function Navbar() {
   const toggleMenu = () => setIsOpen(!isOpen);
   const closeMenu = () => setIsOpen(false);
   return (
-    <nav className="fixed top-0 z-50 w-full bg-bg px-3 py-4 shadow-sm after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:bg-linear-to-r after:from-violet-500 after:to-fuchsia-500">
+    <nav className="sticky top-0 z-50 w-full bg-bg px-3 py-4 shadow-sm after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:bg-linear-to-r after:from-violet-500 after:to-fuchsia-500">
       <div className="mx-auto flex items-center justify-between">
-        <Link
-          to="/"
-          className="fixed text-2xl font-bold text-transparent bg-clip-text bg-linear-to-r from-violet-500 to-fuchsia-500"
-          onClick={closeMenu}
-        >
-          KMQS
-        </Link>
-        <div className="hidden md:flex md:justify-center md:items-center md:mx-auto">
+        <div className="flex-1">
+          <Link
+            to="/"
+            className="text-2xl font-bold text-transparent bg-clip-text bg-linear-to-r from-violet-500 to-fuchsia-500"
+            onClick={closeMenu}
+          >
+            KMQS
+          </Link>
+        </div>
+        <div className="hidden md:block">
           <NavLinks />
         </div>
-        <div className="md:hidden">
-          <MobileMenuButton isOpen={isOpen} onClick={toggleMenu} />
+        <div className="flex flex-1 justify-end">
+          <div className="md:hidden">
+            <MobileMenuButton isOpen={isOpen} onClick={toggleMenu} />
+          </div>
         </div>
       </div>
       <MobileMenuOverlay isOpen={isOpen} onClose={closeMenu}>
