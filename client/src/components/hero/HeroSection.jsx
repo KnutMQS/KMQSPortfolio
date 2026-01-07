5ec661ee40be05f59ce8bd31b5f4ef17cef4887e
KnutMQS <knutmqs@gmail.com>
2026-01-07 10:05:10 +0100

    Refactor HeroSection component to improve structure and maintainability

--- client/src/components/hero/HeroSection.jsx
+++ client/src/components/hero/HeroSection.jsx
@@ -4,18 +4,17 @@ import Button from "../shared/Button";
 function HeroSection() {
   return (
     <section className="relative flex flex-col min-h-screen items-center justify-center px-6 bg-bg overflow-hidden">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-51 h-51 rounded-4xl bg-radial-[at_25%_25%] from-blue-800 to-teal-800 to-75% blur-3xl " />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-51 h-51 rounded-4xl bg-radial-[at_25%_25%] from-blue-800 to-teal-800 to-75% blur-3xl" />
       <HeroContent />
-      <div>
-        <Button variant="primary" size="medium">
-          <a
-            href="https://github.com/KnutMQS/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-        </Button>
+      <div className="mt-4">
+        <a
+          href="https://github.com/KnutMQS/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-bg border-2 border-violet-500 hover:bg-gray-200 hover:text-black focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-inset-2 focus-visible:ring-blue-600 px-4 py-2 text-sm m-2 font-semibold rounded-full transition ease-in-out duration-150"
+        >
+          GitHub
+        </a>
         <Button variant="primary" size="medium">
           E-Mail
         </Button>
