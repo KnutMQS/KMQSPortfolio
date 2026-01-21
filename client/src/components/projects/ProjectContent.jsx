9f0bbd5c2e4fc4d19e47ee5d32d176517a327b4e
KnutMQS <knutmqs@gmail.com>
2026-01-21 12:28:13 +0100

    Refactor layout styles in ProjectContent and ProjectHeader components for improved spacing and alignment

--- client/src/components/projects/ProjectContent.jsx
+++ client/src/components/projects/ProjectContent.jsx
@@ -11,7 +11,7 @@ function ProjectContent() {
     { title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
   ];
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-x-10 gap-y-10  mx-10 aspect-square object-cover">
+    <div className="grid grid-cols-1 md:grid-cols-3 place-content-evenly gap-10 mx-10 object-cover">
       {projects.map((project, index) => (
         <ProjectCard
           key={index}
--- client/src/components/projects/ProjectHeader.jsx
+++ client/src/components/projects/ProjectHeader.jsx
@@ -1,7 +1,7 @@
 function ProjectHeader() {
   return (
     <div className="flex justify-center items-center">
-      <h1 className="p-2 text-3xl md:text-4xl font-bold tracking-tight mb-4 text-transparent bg-clip-text bg-linear-to-r from-violet-500 to-fuchsia-500">
+      <h1 className="p-2 text-3xl md:text-4xl font-bold tracking-tight my-10 text-transparent bg-clip-text bg-linear-to-r from-violet-500 to-fuchsia-500">
         My Projects
       </h1>
     </div>
