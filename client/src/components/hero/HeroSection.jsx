d38a0738384dfb0887c58364252c86e4cffcd64d
KnutMQS <knutmqs@gmail.com>
2026-01-22 11:10:35 +0100

    Update HeroSection to use max-h-screen and h-fit for improved layout consistency

--- client/src/components/hero/HeroSection.jsx
+++ client/src/components/hero/HeroSection.jsx
@@ -3,7 +3,7 @@ import Button from "../shared/Button";
 
 function HeroSection() {
   return (
-    <section className="relative flex flex-col min-h-screen items-center justify-center px-6 bg-bg overflow-hidden">
+    <section className="relative flex flex-col max-h-screen h-fit items-center justify-center px-6 pt-20 bg-bg overflow-hidden">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-51 h-51 rounded-4xl bg-radial-[at_25%_25%] from-blue-800 to-teal-800 to-75% blur-3xl" />
       <HeroContent />
       <div className="mt-4">
