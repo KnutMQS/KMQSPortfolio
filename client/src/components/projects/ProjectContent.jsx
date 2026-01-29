f09e02e316f7ff83e39065553a5eba30cbb98272
KnutMQS <knutmqs@gmail.com>
2026-01-29 12:31:43 +0100

    Refactor ProjectContent to use external projects data file for improved maintainability

--- client/src/components/projects/ProjectContent.jsx
+++ client/src/components/projects/ProjectContent.jsx
@@ -1,20 +1,13 @@
-import kurt from "../../assets/kurtkårejohnny.jpg";
 import ProjectCard from "./ProjectCard";
+import projects from "../../data/data";
 
 function ProjectContent() {
-  const projects = [
-    { title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
-    { title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
-    { title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
-    { title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
-    { title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
-    { title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
-  ];
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 place-content-evenly gap-10 mx-10 object-cover">
       {projects.map((project, index) => (
         <ProjectCard
           key={index}
+          id={project.id}
           title={project.title}
           imageSrc={project.imageSrc}
           imageAlt={project.imageAlt}
---
+++ client/src/data/data.js
@@ -0,0 +1,12 @@
+import kurt from "../assets/kurtkårejohnny.jpg";
+
+const projects = [
+  { id: 1, title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
+  { id: 2, title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
+  { id: 3, title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
+  { id: 4, title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
+  { id: 5, title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
+  { id: 6, title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
+];
+
+export default projects;
