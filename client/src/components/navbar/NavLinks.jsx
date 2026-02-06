a2d1e9dabe967f154cece9d149d4eaedbb44cef9
KnutMQS <knutmqs@gmail.com>
2026-02-06 14:47:08 +0100

    Refactor navigation components to use react-scroll for smooth scrolling and update HeroSection with an ID for linking

--- client/src/components/navbar/NavLinks.jsx
+++ client/src/components/navbar/NavLinks.jsx
@@ -1,33 +1,35 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-scroll";
 
-const getLinkClass = ({ isActive }) =>
-  `rounded-md px-1 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-inset-2 focus-visible:ring-blue-600 transition-all duration-200 ${
-    isActive
-      ? "text-fuchsia-500 font-bold"
-      : "text-gray-400  hover:text-fuchsia-500"
-  }`;
+const linkClass =
+  "text-gray-400 hover:text-fuchsia-500 rounded-md px-1 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-inset-2 focus-visible:ring-blue-600 transition-all duration-200 cursor-pointer";
+const activeLinkClass = "text-fuchsia-500 font-bold";
 
 function NavLinks({
   className = "flex gap-4 text-lg justify-center items-center",
   onClick,
 }) {
   const links = [
-    { to: "/", text: "Home" },
-    { to: "/projects", text: "Projects" },
-    { to: "/about", text: "About" },
+    { to: "home", text: "Home" },
+    { to: "projects", text: "Projects" },
+    { to: "about", text: "About" },
   ];
 
   return (
     <div className={className}>
       {links.map((link) => (
-        <NavLink
+        <Link
           key={link.to}
           to={link.to}
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
           onClick={onClick}
-          className={getLinkClass}
+          className={linkClass}
+          activeClass={activeLinkClass}
         >
           {link.text}
-        </NavLink>
+        </Link>
       ))}
     </div>
   );
