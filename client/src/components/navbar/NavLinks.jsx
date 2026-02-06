df60db4dda0f6d82f03b632c430fee23afb3f1c2
KnutMQS <knutmqs@gmail.com>
2026-02-06 15:07:39 +0100

    Remove 'About' link from NavLinks and simplify linkClass styling

--- client/src/components/navbar/NavLinks.jsx
+++ client/src/components/navbar/NavLinks.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-scroll";
 
 const linkClass =
-  "text-gray-400 hover:text-fuchsia-500 rounded-md px-1 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-inset-2 focus-visible:ring-blue-600 transition-all duration-200 cursor-pointer";
+  "hover:text-fuchsia-500 rounded-md px-1 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-inset-2 focus-visible:ring-blue-600 transition-all duration-200 cursor-pointer";
 const activeLinkClass = "text-fuchsia-500 font-bold";
 
 function NavLinks({
@@ -11,7 +11,6 @@ function NavLinks({
   const links = [
     { to: "home", text: "Home" },
     { to: "projects", text: "Projects" },
-    { to: "about", text: "About" },
   ];
 
   return (
