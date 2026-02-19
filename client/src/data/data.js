243b889336c536c6229cfa69df14c5ee62e31408
KnutMQS <knutmqs@gmail.com>
2026-02-19 13:17:16 +0100

    Added a second picture to make it easier to see if carousel works

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,12 +1,13 @@
 import kurt from "../assets/kurtkårejohnny.jpg";
+import tshirt from "../assets/deathtrip.png";
 
 const projects = [
   { id: 1, title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
-  { id: 2, title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
+  { id: 2, title: "Insert Slogan Here", imageSrc: tshirt, imageAlt: "Slogan" },
   { id: 3, title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
-  { id: 4, title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
+  { id: 4, title: "Insert Slogan Here", imageSrc: tshirt, imageAlt: "Slogan" },
   { id: 5, title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
-  { id: 6, title: "Kurt Kåre Johnny", imageSrc: kurt, imageAlt: "The legend" },
+  { id: 6, title: "Insert Slogan Here", imageSrc: tshirt, imageAlt: "Slogan" },
 ];
 
 export default projects;
