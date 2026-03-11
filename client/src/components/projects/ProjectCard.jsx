ad99229ee54d0df30ce2e352483ec8868b27115b
KnutMQS <knutmqs@gmail.com>
2026-03-11 12:37:55 +0100

    Fixed some scaling issues with images in the carousel

--- client/src/components/projects/ProjectCard.jsx
+++ client/src/components/projects/ProjectCard.jsx
@@ -4,12 +4,14 @@ const ProjectCard = ({ title, imageSrc, imageAlt }) => {
       <span className="flex justify-center items-center font-bold text-xl text-gray-200 m-2">
         {title}
       </span>
-      <img
-        src={imageSrc}
-        alt={imageAlt}
-        loading="lazy"
-        className="w-auto h-auto rounded-3xl drop-shadow-[0_0_11px_rgba(162,28,175,1)]"
-      />
+      <div className="w-full h-100 object-fit">
+        <img
+          src={imageSrc}
+          alt={imageAlt}
+          loading="lazy"
+          className="rounded-3xl drop-shadow-[0_0_11px_rgba(162,28,175,1)]"
+        />
+      </div>
     </div>
   );
 };
